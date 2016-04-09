d34c227fead128fcc9ce1f156207d0ff318ec3a1
Oshomo Oforomeh <oshomo.oforomeh@konga.com>
2016-04-09 17:38:31 +0100

    Updated LazyRegex to return reges expression Updated Test to include new changes

--- test/index.js
+++ test/index.js
@@ -3,22 +3,34 @@
 const chai  = require('chai');
 const should = chai.should();
 const LazyRegex = require("../index.js");
-const LazyRegexp = LazyRegex();
 const OneOrMore = LazyRegex();
 const Log = LazyRegex();
 const ZeroOrMore = LazyRegex();
+const oneorMoreAndZeroOrMore = LazyRegex();
 
 describe('#LazyRegex', function() {
-  it('return Hello World', function() {
-    Log.log('Hello World').compile().should.equal('Hello World');
+  it('return /Hello World/', function() {
+    Log
+		.log('Hello World')
+		.compile().should.equal('/Hello World/');
   });
   
-  it('return b*', function() {
-    ZeroOrMore.zeroOrMore('b').compile().should.equal('b*');
-    LazyRegexp.log('Hello World').compile().should.equal('Hello World');
+  it('return /b*/', function() {
+    ZeroOrMore
+		.zeroOrMore('b')
+		.compile().should.equal('/b*/');
   });
 
-  it('return a+', function() {
-    OneOrMore.oneOrMore("a").compile().should.equal('a+');
+  it('return /a+/', function() {
+    OneOrMore
+		.oneOrMore("a")
+		.compile().should.equal('/a+/');
+  });
+  
+  it('return /a+b*/', function() {
+    oneorMoreAndZeroOrMore
+		.oneOrMore("a")
+		.zeroOrMore("b")
+		.compile().should.equal('/a+b*/');
   });
 });
