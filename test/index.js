f01bbc303ae4c6e6cfc16577d5d298ac6fdd0f0d
oluwatobi.okusanya <sanya_tobi@yahoo.com>
2016-04-09 16:53:47 +0100

    Merge branch 'master' of https://github.com/stuckcoders-app/lazy-regex
    # Conflicts:
    #	lib/lazy-regex.js
    #	lib/regex/regex.json
    #	test/index.js

--- test/index.js
+++ test/index.js
@@ -17,9 +17,7 @@ describe('#LazyRegex', function() {
     ZeroOrMore.zeroOrMore('b').compile().should.equal('b*');
     LazyRegexp.log('Hello World').compile().should.equal('Hello World');
   });
-});
 
-describe('#oneOrMore', function() {
   it('return a+', function() {
     OneOrMore.oneOrMore("a").compile().should.equal('a+');
   });
