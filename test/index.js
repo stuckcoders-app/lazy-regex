f485ea4dc28635a93ca9e698d1b79ce000742fd3
oluwatobi.okusanya <sanya_tobi@yahoo.com>
2016-04-09 16:48:50 +0100

    Added oneormore to rejex.json Added oneormore function to lazyregex Added test for oneormore function

--- test/index.js
+++ test/index.js
@@ -4,9 +4,16 @@ const chai  = require('chai');
 const should = chai.should();
 const LazyRegex = require("../index.js");
 const LazyRegexp = LazyRegex();
+const OneOrMore = LazyRegex();
 
 describe('#escape', function() {
   it('return Hello World', function() {
-    LazyRegexp.log('Hello World').should.equal('Hello World');
+    LazyRegexp.log('Hello World').compile().should.equal('Hello World');
+  });
+});
+
+describe('#oneOrMore', function() {
+  it('return a+', function() {
+    OneOrMore.oneOrMore("a").compile().should.equal('a+');
   });
 });
